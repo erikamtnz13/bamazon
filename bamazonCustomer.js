bdc68f05c8e9474db837aa875bc03a6b6c251cc2
Erika Martinez <erikamartinez@Erikas-MacBook-Pro.local>
2017-09-12 23:31:14 -0700

    adding changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,27 +1,89 @@
 var mysql = require("mysql");
-var require = require("inquirer");
+var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: 'localhost',
     port: 8889,
-
-    user: "root",
-    password: "root",
-    database: "greatBay_DB"
+    user: 'root',
+    password: 'root',
+    database: 'bamazon_db'
 });
 
-connection.connect(function(error){
-    if (error) throw error;
-
-    start();
+connection.connect(function (error) {
+    if (error) {
+        console.error('connection error: ' + error.stack);
+        return;
+    }
+    console.log("--------------");
+    getData();
 });
 
-function start(){
-    inquirer
-    .prompt({
-        name: "selectItem",
-        type: "rawlist",
-        message:"Please select the product you would like to purchase:",
-        choices: 
-    })
+function getData() {
+    connection.query("SELECT * FROM products", function (error, data) {
+        if (error) throw error;
+        getUserPrompt(data);
+    });
+}
+
+function getUserPrompt(data) {
+    var options = [];
+    for (d in data) {
+        options[d] = data[d].item_id + " : " + data[d].product_name;
+    }
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "productId",
+            message: "Which products would you like to buy?",
+            choices: options,
+        },
+        {
+            type: "input",
+            name: "quantity",
+            message: "How many product units would you like to buy?"
+        }
+    ]).then(function (userInput) {
+
+        var product = userInput.productId.split(" :");
+        var productId = product[0];
+
+        connection.query("SELECT * FROM products WHERE ?",
+            [{ item_id: productId }],
+            function (error, data) {
+                if (error) throw error;
+                checkStock(data, userInput.quantity);
+            });
+    });
+
+    function checkStock(data, quantity) {
+        if (data[0].stock_quantity < quantity) {
+            console.log("Insufficient quantity!");
+            connection.end();
+        } else {
+            updateDB(data, quantity);
+            totalCost(data, quantity);
+        }
+    }
+
+    function updateDB(data, quantity) {
+        var quantity_left = data[0].stock_quantity - quantity;
+        connection.query("UPDATE products SET ? WHERE ?",
+            [
+                { stock_quantity: quantity_left },
+                { item_id: data[0].item_id }
+            ],
+            function (error, data) {
+                if (error) throw error;
+            });
+    }
+
+    function totalCost(data, quantity) {
+        console.log("\n=============\n");
+        console.log("Product : " + data[0].product_name);
+        console.log("Price \t: " + data[0].price);
+        console.log("Quantity :" + quantity + "\n");
+        console.log("Total Cost: " + data[0].price * quantity)
+        connection.end();
+    }
+
 }
